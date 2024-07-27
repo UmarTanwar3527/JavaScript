968ebb5d7cffe4fe40507802dda1f6bb6195c476
Mohammed Umar Tanwar <umartanwar11651913@gmail.com>
2024-07-27 13:38:51 +0530

    code updated

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Umar Tanwar")
 
 
-let name = "hitesh"
+let name = "Umar Tanwar"
 let age = 18
 let isLoggedIn = false
 let state;
